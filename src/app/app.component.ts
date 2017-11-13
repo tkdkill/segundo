095fb325c016b4731a022f56df65944073dcfa34
António Pinho <antonio.henrique@ua.pt>
2017-11-13 19:44:27 +0000

    13-11-2017_Aula26PT3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  // variáveis que controlam a visibilidade dos componentes
+  loja_visivel: boolean = true;
+  stock_visivel: boolean = false;
+
+  mostrarLoja() {
+    // apresenta loja e remove o stock
+    this.loja_visivel = true;
+    this.stock_visivel = false;
+
+  }
+  mostrarStock() {
+    // apresenta stock e remove a loja
+    this.loja_visivel = false;
+    this.stock_visivel = true;
+  }
 }
